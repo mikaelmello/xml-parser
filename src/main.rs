44ccd643cf6aea336e9625e14ac29117f058c642
Mikael Mello <git@mikaelmello.com>
2021-09-06 21:18:04 -0300

    add literal_parser combinator
    Signed-off-by: Mikael Mello <git@mikaelmello.com>

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,25 @@ fn the_letter_a(input: &str) -> Result<(&str, ()), &str> {
     }
 }
 
+fn match_literal(expected: &'static str) -> impl Fn(&str) -> Result<(&str, ()), &str> {
+    move |input| match input.get(0..expected.len()) {
+        Some(next) if next == expected => Ok((&input[expected.len()..], ())),
+        _ => Err(input),
+    }
+}
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[test]
+fn literal_parser() {
+    let parse_joe = match_literal("Hello Joe!");
+
+    assert_eq!(Ok(("", ())), parse_joe("Hello Joe!"));
+    assert_eq!(
+        Ok((" Hello Robert!", ())),
+        parse_joe("Hello Joe! Hello Robert!")
+    );
+    assert_eq!(Err("Hello Mike!"), parse_joe("Hello Mike!"));
 }
