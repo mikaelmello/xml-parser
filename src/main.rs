e502b00fae0ec74cbed9404099cce8099597394e
Mikael Mello <git@mikaelmello.com>
2021-09-06 21:25:17 -0300

    add identifier parser
    Signed-off-by: Mikael Mello <git@mikaelmello.com>

--- src/main.rs
+++ src/main.rs
@@ -19,18 +19,63 @@ fn match_literal(expected: &'static str) -> impl Fn(&str) -> Result<(&str, ()), 
     }
 }
 
+fn identifier(input: &str) -> Result<(&str, String), &str> {
+    let mut matched = String::new();
+    let mut chars = input.chars();
+
+    match chars.next() {
+        Some(next) if next.is_alphabetic() => matched.push(next),
+        _ => return Err(input),
+    }
+
+    while let Some(next) = chars.next() {
+        if next.is_alphanumeric() || next == '-' {
+            matched.push(next);
+        } else {
+            break;
+        }
+    }
+
+    let next_index = matched.len();
+
+    Ok((&input[next_index..], matched))
+}
+
 fn main() {
     println!("Hello, world!");
 }
 
-#[test]
-fn literal_parser() {
-    let parse_joe = match_literal("Hello Joe!");
+#[cfg(test)]
+mod test {
+    use crate::{identifier, match_literal};
 
-    assert_eq!(Ok(("", ())), parse_joe("Hello Joe!"));
-    assert_eq!(
-        Ok((" Hello Robert!", ())),
-        parse_joe("Hello Joe! Hello Robert!")
-    );
-    assert_eq!(Err("Hello Mike!"), parse_joe("Hello Mike!"));
+    #[test]
+    fn literal_parser() {
+        let parse_joe = match_literal("Hello Joe!");
+
+        assert_eq!(Ok(("", ())), parse_joe("Hello Joe!"));
+        assert_eq!(
+            Ok((" Hello Robert!", ())),
+            parse_joe("Hello Joe! Hello Robert!")
+        );
+        assert_eq!(Err("Hello Mike!"), parse_joe("Hello Mike!"));
+    }
+
+    #[test]
+    fn identifier_parser() {
+        assert_eq!(
+            Ok(("", "i-am-an-identifier".to_string())),
+            identifier("i-am-an-identifier")
+        );
+
+        assert_eq!(
+            Ok((" entirely an identifier", "not".to_string())),
+            identifier("not entirely an identifier")
+        );
+
+        assert_eq!(
+            Err("!not at all an identifier"),
+            identifier("!not at all an identifier")
+        );
+    }
 }
