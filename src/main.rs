9e5f976aa1fe89a48671f8261fd984818410fdf4
Mikael Mello <git@mikaelmello.com>
2021-09-06 21:15:10 -0300

    add the_letter_a parser
    Signed-off-by: Mikael Mello <git@mikaelmello.com>

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,13 @@ struct Element {
     children: Vec<Element>,
 }
 
+fn the_letter_a(input: &str) -> Result<(&str, ()), &str> {
+    match input.chars().next() {
+        Some('a') => Ok((&input['a'.len_utf8()..], ())),
+        _ => Err(input),
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
