b1d7a7b46027e57a610f4ff4f0c98d9bac708692
Mikael Mello <git@mikaelmello.com>
2021-09-06 21:29:43 -0300

    add pair combinator
    Signed-off-by: Mikael Mello <git@mikaelmello.com>

--- src/main.rs
+++ src/main.rs
@@ -34,13 +34,27 @@ fn identifier(input: &str) -> Result<(&str, String), &str> {
     Ok((&input[next_index..], matched))
 }
 
+fn pair<P1, P2, R1, R2>(parser1: P1, parser2: P2) -> impl Fn(&str) -> Result<(&str, (R1, R2)), &str>
+where
+    P1: Fn(&str) -> Result<(&str, R1), &str>,
+    P2: Fn(&str) -> Result<(&str, R2), &str>,
+{
+    move |input| match parser1(input) {
+        Ok((next_input, result1)) => match parser2(next_input) {
+            Ok((final_input, result2)) => Ok((final_input, (result1, result2))),
+            Err(err) => Err(err),
+        },
+        Err(err) => Err(err),
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
 
 #[cfg(test)]
 mod test {
-    use crate::{identifier, match_literal};
+    use crate::{identifier, match_literal, pair};
 
     #[test]
     fn literal_parser() {
@@ -69,6 +83,15 @@ mod test {
         assert_eq!(
             Err("!not at all an identifier"),
             identifier("!not at all an identifier")
+        );
+    }
+
+    #[test]
+    fn pair_combinator() {
+        let tag_opener = pair(match_literal("<"), identifier);
+        assert_eq!(
+            Ok(("/>", ((), "my-first-element".to_string()))),
+            tag_opener("<my-first-element/>")
         );
     }
 }
