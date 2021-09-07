a8dade9e569fb933d622214e14aed7d0d264c177
Mikael Mello <git@mikaelmello.com>
2021-09-06 21:11:54 -0300

    add Element struct
    Signed-off-by: Mikael Mello <git@mikaelmello.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+#[derive(Clone, Debug, PartialEq, Eq)]
+struct Element {
+    name: String,
+    attributes: Vec<(String, String)>,
+    children: Vec<Element>,
+}
+
 fn main() {
     println!("Hello, world!");
 }
