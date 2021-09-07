716fe9d1607a76eb3750a2d20f87d53436ebde34
Mikael Mello <git@mikaelmello.com>
2021-09-06 21:50:59 -0300

    add whitespace parsers
    Signed-off-by: Mikael Mello <git@mikaelmello.com>

--- src/main.rs
+++ src/main.rs
@@ -87,6 +87,18 @@ where
     }
 }
 
+fn whitespace_char<'a>() -> impl Parser<'a, char> {
+    pred(any_char, |c| c.is_whitespace())
+}
+
+fn space1<'a>() -> impl Parser<'a, Vec<char>> {
+    one_or_more(whitespace_char())
+}
+
+fn space0<'a>() -> impl Parser<'a, Vec<char>> {
+    zero_or_more(whitespace_char())
+}
+
 fn one_or_more<'a, P, A>(parser: P) -> impl Parser<'a, Vec<A>>
 where
     P: Parser<'a, A>,
