54227eca4aa6eccfbf654208944a876ad24d1234
Mikael Mello <git@mikaelmello.com>
2021-09-06 21:25:40 -0300

    remove 'the_letter_a' parser
    Signed-off-by: Mikael Mello <git@mikaelmello.com>

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,6 @@ struct Element {
     children: Vec<Element>,
 }
 
-fn the_letter_a(input: &str) -> Result<(&str, ()), &str> {
-    match input.chars().next() {
-        Some('a') => Ok((&input['a'.len_utf8()..], ())),
-        _ => Err(input),
-    }
-}
-
 fn match_literal(expected: &'static str) -> impl Fn(&str) -> Result<(&str, ()), &str> {
     move |input: &str| match input.strip_prefix(expected) {
         Some(next) => Ok((next, ())),
